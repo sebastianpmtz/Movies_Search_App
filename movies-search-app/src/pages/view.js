eadfd72a5431c79933669e0958f4baa5985fefed
sebastianpmtz@gmail.com <sebastianpmtz@gmail.com>
2022-12-01 15:47:28 -0600

    Movies-search-app completed

---
+++ movies-search-app/src/pages/view.js
@@ -0,0 +1,45 @@
+import Layout from "../components/layout";
+import { useAppContext } from "../store/store";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function View() {
+  const [item, setItem] = useState(null);
+  const params = useParams();
+  const store = useAppContext();
+
+  const itemStyles = {
+    container: {
+      display: 'flex',
+      gap: '20px',
+      color: 'white',
+      width: '800px',
+      margin:'0 auto'
+    }
+  }
+
+  useEffect(() => {
+    const book = store.getItem(params.bookId);
+    setItem(book);
+  }, []);
+  if (!item) {
+    return <Layout>Item not found</Layout>
+  }
+
+  return (
+    <Layout >
+      <div style={itemStyles.container}>
+        <div>
+          <div>{item?.cover ? <img src={item?.cover} width='400' /> : ''}</div>
+        </div>
+        <div>
+          <h2>{item?.title}</h2>
+          <div>{item?.author}</div>
+          <div>{item?.intro}</div>
+          <div>{item?.completed ? 'Finished' : 'Unfinished'}</div>
+          <div>{ item?.review }</div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
